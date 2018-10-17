bc1462126c31626362eb31a5871d1e5d2d57c064
lanlan <2665482180@qq.com>
2018-10-17 10:56:56 +0800

    first day

---
+++ src/com/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+	@RequestMapping("/toIndex")
+	public String showIndex() {
+		return "/index";
+	}
+	
+	@RequestMapping("{toPage}")
+	public String showAll(@PathVariable String toPage) {
+		return toPage;
+	}
+}
